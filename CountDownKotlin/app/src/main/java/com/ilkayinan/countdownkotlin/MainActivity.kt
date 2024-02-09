164c04b4be497e9321ae94909f6152241b4b2f5c
ilkayinan <116514109+ilkayinan@users.noreply.github.com>
2024-02-09 18:11:33 +0300

    refresh

---
+++ CountDownKotlin/app/src/main/java/com/ilkayinan/countdownkotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ilkayinan.countdownkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.ilkayinan.countdownkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        object : CountDownTimer(10000,1000){
+            override fun onTick(p0: Long) {
+                binding.textView.text = "Left: ${p0/1000}"
+            }
+
+            override fun onFinish() {
+                binding.textView.text = "Left = 0"
+            }
+
+        }.start()
+    }
+}
